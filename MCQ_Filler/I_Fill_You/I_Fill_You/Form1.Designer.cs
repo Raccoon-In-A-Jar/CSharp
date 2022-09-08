ac6eb2d646fcb8291d4beab231b18f9213e47844
Raccoon-In-A-Jar <108840662+Raccoon-In-A-Jar@users.noreply.github.com>
2022-09-09 00:41:05 +0200

    Add files via upload

---
+++ MCQ_Filler/I_Fill_You/I_Fill_You/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace I_Fill_You
+{
+    partial class MCQ_Filler
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Remplir = new System.Windows.Forms.Button();
+            this.fichier = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Remplir
+            // 
+            this.Remplir.Location = new System.Drawing.Point(53, 399);
+            this.Remplir.Name = "Remplir";
+            this.Remplir.Size = new System.Drawing.Size(75, 23);
+            this.Remplir.TabIndex = 0;
+            this.Remplir.Text = "Remplir";
+            this.Remplir.UseVisualStyleBackColor = true;
+            this.Remplir.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // fichier
+            // 
+            this.fichier.Location = new System.Drawing.Point(100, 46);
+            this.fichier.Name = "fichier";
+            this.fichier.Size = new System.Drawing.Size(114, 23);
+            this.fichier.TabIndex = 1;
+            this.fichier.Text = "Choisir le fichier";
+            this.fichier.UseVisualStyleBackColor = true;
+            this.fichier.Click += new System.EventHandler(this.fichier_Click);
+            // 
+            // MCQ_Filler
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(345, 450);
+            this.Controls.Add(this.fichier);
+            this.Controls.Add(this.Remplir);
+            this.Name = "MCQ_Filler";
+            this.Text = "Remplisseur de QCM";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Remplir;
+        private System.Windows.Forms.Button fichier;
+    }
+}
+
---
+++ MCQ_Filler/I_Fill_You/I_Fill_You/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Office.Core;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace I_Fill_You
+{
+    public partial class MCQ_Filler : Form
+    {
+        public MCQ_Filler()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Excel.Application excelApp = new Excel.Application();
+            string myPath = @"E:\filler.xlsx";
+            excelApp.Workbooks.Open(myPath);
+            int rowIndex = 1; int colIndex = 1;
+
+            excelApp.Cells[rowIndex, colIndex] = "First";
+            excelApp.Visible = true;
+        }
+
+        OpenFileDialog quizzFile = new OpenFileDialog();
+        private void fichier_Click(object sender, EventArgs e)
+        {
+            //DialogResult result = openFileDialog1.ShowDialog(); // Show the dialog.
+
+            quizzFile.ShowDialog();
+        }
+    }
+}
